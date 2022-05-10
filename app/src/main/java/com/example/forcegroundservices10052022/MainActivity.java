9516983708f6bc655771fc780e75995626d9b2bf
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-05-10 17:24:05 +0700

    create 2 button and findbyid

--- app/src/main/java/com/example/forcegroundservices10052022/MainActivity.java
+++ app/src/main/java/com/example/forcegroundservices10052022/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.forcegroundservices10052022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtnStartServices, mBtnStopServices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        mBtnStartServices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void init()
+    {
+        mBtnStartServices=findViewById(R.id.btnStartServices);
+        mBtnStopServices=findViewById(R.id.btnStopServices);
     }
 }
